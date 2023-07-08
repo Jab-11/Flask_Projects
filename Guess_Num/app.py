87a1b6c6061c3d5454ab632e71e7cecaf8db149e
cjap1110 <115206504+cjap1110@users.noreply.github.com>
2023-07-08 11:28:10 +0000

    made some changes

--- Guess_Num/app.py
+++ Guess_Num/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,26 @@ app = Flask(__name__)
 def home():
   return render_template("home.html")
 
-@app.route("/guess")
-def guess():
-  return render_template("guess.html")
+@app.route('/game', methods=['POST'])
+def start_game():
+    range_value = request.form['range']
+    return render_template('guess.html', range_value=range_value)
+
+@app.route('/guess', methods=['POST'])
+def check_guess():
+    target_number = int(request.form['target_number'])
+    user_guess = int(request.form['guess'])
+    range_value = int(request.form['range_value'])
+    feedback = ""
+
+    if user_guess < target_number:
+        feedback = "Guessed too low!"
+    elif user_guess > target_number:
+        feedback = "Guessed too high!"
+    else:
+        feedback = "Congratulations! You guessed correctly."
+
+    return render_template('guess.html', range_value=range_value, target_number=target_number, feedback=feedback)
   
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
