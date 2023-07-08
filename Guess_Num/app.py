45be0ef0fab8c0bef8235568f2dc30f256b687d3
cjap1110 <115206504+cjap1110@users.noreply.github.com>
2023-07-08 06:11:15 +0000

    added second page and navbar

--- Guess_Num/app.py
+++ Guess_Num/app.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def sayhello():
+def home():
   return render_template("home.html")
 
+@app.route("/guess")
+def guess():
+  return render_template("guess.html")
+  
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
